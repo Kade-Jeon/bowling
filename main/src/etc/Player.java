514f7c0037a75fc6995a88ceaeb1210a5b939c10
kade <zsc0248@gmail.com>
2024-02-25 21:02:46 +0900

    feat: 플레이어 객체 생성

---
+++ main/src/etc/Player.java
@@ -0,0 +1,44 @@
+package etc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+    private String name;
+    private Scoreboard scoreboard;
+    private List<Integer> scores;
+    public Player(String name, Scoreboard scoreboard) {
+        this.name = name;
+        this.scoreboard = scoreboard;
+        this.scores = new ArrayList<>(10);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Scoreboard getScoreboard() {
+        return scoreboard;
+    }
+
+    public void setScoreboard(Scoreboard scoreboard) {
+        this.scoreboard = scoreboard;
+    }
+
+    public List<Integer> getScores() {
+        return scores;
+    }
+
+    public void setScores(List<Integer> scores) {
+        this.scores = scores;
+    }
+
+    public void showScoreboard() {
+        System.out.println(scoreboard.getScoreboard().subList(0, 10));
+    }
+
+}
