e1755b49dbdc4b80e89c0808a4cfdfe1d17f8ee5
kade <zsc0248@gmail.com>
2024-02-29 10:10:47 +0900

    delete: 불필요 클래스 삭제

--- main/src/Exceptions/InputErrorException.java
+++
@@ -1,9 +0,0 @@
-package Exceptions;
-
-import java.util.InputMismatchException;
-
-public class InputErrorException extends InputMismatchException {
-    public InputErrorException(String message) {
-        super(message);
-    }
-}
